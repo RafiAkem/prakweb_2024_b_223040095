3adf4a0429e251ccc76b9c2098781c65f6b4ea14
RafiAkem <yc66zio@gmail.com>
2024-10-19 00:31:06 +0700

    Video ke-6 Assets(MVC)

--- phpmvc/app/views/About/index.php
+++ phpmvc/app/views/About/index.php
@@ -1,2 +1,7 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+
+<h1 class="mt-4">About Me</h1>
+<img src="<?= BASEURL; ?>/img/profile.png" alt="Rafi" width="200" class="rounded shadow">
+<p>Halo, nama saya <?=$data['nama'];?>, umur saya <?= $data['umur']; ?>. Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+</div>
